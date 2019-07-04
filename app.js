ca1a7ed74892bd8acf123f67438c3380381c2bdd
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-04 08:06:17 -0700

    Added output to page functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokesFromAPI);
 let jokeData = [];
 function getJokesFromAPI(e) {
-  console.log('API content here')
+  //console.log('API content here')
 
   const numberOfJokes = document.getElementById('numberOfJokes');
   //console.log(numberOfJokes.value);
@@ -19,12 +19,23 @@ function getJokesFromAPI(e) {
   //     console.log(data);
   // })
   // e.preventDefault();
-
+  let output = '';
 
   //with arrow functions
-  fetch(endpoint) 
-    .then(res =>res.json())
-    .then(data => console.log(data))
+  fetch(endpoint)
+    .then(res => res.json())
+    //.then(data => console.log(data))
+     .then(function(data) {
+       if (data.type === 'success') {
+         data.value.forEach(function(param){
+           output += `<li>${param.joke}</li>`
+        })
+       }
+       document.querySelector('.jokes').innerHTML = output;
+    })
+    .catch(function(err) {
+    console.warn(`There is an error fetching data! ERROR: ${err}`)
+  })
   e.preventDefault();
 
 }
